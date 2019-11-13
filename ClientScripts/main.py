4c9bb5535e661054e508dc65b0878cb70376483f
shashank-simha <shashanksimha183@gmail.com>
2019-11-13 23:50:22 +0530

    Fix syntax errors

--- ClientScripts/main.py
+++ ClientScripts/main.py
@@ -12,7 +12,8 @@ button = 16             # Push button
 def setup():
     GPIO.setwarnings(False)			# disable warnings
     GPIO.setmode(GPIO.BOARD)		# set pin numbering systemGPIO.setup(ledpin,GPIO.OUT)
-    GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)) # setup input pin with pull up config
+    GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP) # setup input pin with pull up config
+    GPIO.setup(ledpin,GPIO.OUT)
     pi_pwm = GPIO.PWM(ledpin,1000)		# create PWM instance with frequency
     pi_pwm.start(0)				# start PWM of required Duty Cycle 
 
@@ -67,7 +68,6 @@ def ControlSpeed():
         else:
             pass
         
-setup() # call setup function
 
 GetMaxSpeedThread = Thread(target=GetMaxSpeed)
 GetMaxSpeedThread.start()
@@ -76,5 +76,6 @@ ControlSpeedThread = Thread(target=ControlSpeed)
 ControlSpeedThread.start()
 
 if __name__ == "__main__":
+    setup() # call setup function
     while (1):
         pass
